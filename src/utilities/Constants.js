5dcd9aa47f6a36ff3f69e8a11845e285f813d7f8
DHVSGG <diego@qapla.gg>
2021-02-05 18:46:28 -0600

    TWITCH_REDIRECT_URI constant updated

--- src/utilities/Constants.js
+++ src/utilities/Constants.js
@@ -7,4 +7,4 @@ export const TWITCH_SECRET_ID = 'xt2ed1xz00cwglz34fu2m95k77xnj6';
 /**
  * Twitch Utils
  */
-export const TWITCH_REDIRECT_URI = process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://qaplastreamerdashboard.firebaseapp.com';
+export const TWITCH_REDIRECT_URI = !process.env.NODE_ENV || process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://dashboard.qapla.gg/';

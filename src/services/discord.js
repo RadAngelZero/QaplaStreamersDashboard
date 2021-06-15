7a2d2456c1d4c6912bbf40e046ae8a3694cc9023
DHVSGG <diego@qapla.gg>
2021-06-15 17:11:07 -0500

    Discord api direction updated

--- src/services/discord.js
+++ src/services/discord.js
@@ -1,7 +1,7 @@
 
 export async function notifyUpdateToQaplaAdmins(streamId, streamerName, newDateObject = new Date()) {
     await fetch(
-        'https://discordapp.com/api/webhooks/692800624017801287/E68ViYaO34VMvCdfiRuADcTVSo_B-azRz0RiSo_L6EU39gAofnDTuOuiyWEYHbbMGQZE',
+		'https://discord.com/api/webhooks/854481795700490280/qCl8z1-IDV6WQ-teWP69zDMULfFJ3jJb_brQYbUA1XiW7o2kE6EkXKI_k5ayU-HP64Ho',
         {
             method: 'POST',
             headers: {
@@ -9,8 +9,8 @@ export async function notifyUpdateToQaplaAdmins(streamId, streamerName, newDateO
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				content: `Evento de ${streamerName} ha cambiado de fecha\nNueva fecha: ${newDateObject.toLocaleString('es-mx')}\nID del evento: ${streamId}\n<@586019077546442752>`,
-            	username: "Qapla Match Announcer",
+				content: `Evento de ${streamerName} ha cambiado de fecha\nNueva fecha: ${newDateObject.toLocaleString('es-mx')}\nID del evento: ${streamId}\n<@524674047456444416>`,
+            	username: "Qapla Date Notifier",
 				tts: false
 			})
         }

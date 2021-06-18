a63df5ef18704f694eceaa9771275159b908e49a
DHVSGG <diego@qapla.gg>
2021-06-18 17:58:33 -0500

    Tag list of admins

--- src/services/discord.js
+++ src/services/discord.js
@@ -1,5 +1,18 @@
 
+/**
+ * Send a message to an admin Qapla discord server with the information about the update
+ * @param {string} streamId Id of the streamer
+ * @param {string} streamerName Name of the streamer
+ * @param {Date} newDateObject Date information to notify to admins
+ */
 export async function notifyUpdateToQaplaAdmins(streamId, streamerName, newDateObject = new Date()) {
+	let adminsToTagOnDiscordMessage = ['524674047456444416', '630187691757273119'];
+
+	let adminTags = '';
+	adminsToTagOnDiscordMessage.forEach((adminId) => {
+		adminTags += `<@${adminId}>`;
+	});
+
     await fetch(
 		'https://discord.com/api/webhooks/854481795700490280/qCl8z1-IDV6WQ-teWP69zDMULfFJ3jJb_brQYbUA1XiW7o2kE6EkXKI_k5ayU-HP64Ho',
         {
@@ -9,7 +22,7 @@ export async function notifyUpdateToQaplaAdmins(streamId, streamerName, newDateO
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify({
-				content: `Evento de ${streamerName} ha cambiado de fecha\nNueva fecha: ${newDateObject.toLocaleString('es-mx')}\nID del evento: ${streamId}\n<@524674047456444416>`,
+				content: `Evento de ${streamerName} ha cambiado de fecha\nNueva fecha: ${newDateObject.toLocaleString('es-mx')}\nID del evento: ${streamId}\n${adminTags}`,
             	username: "Qapla Date Notifier",
 				tts: false
 			})

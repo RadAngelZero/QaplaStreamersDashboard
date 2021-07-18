aeae950c80651da5314f40c85bd86b9dbe6ca19e
RadAngelQapla <angel@qapla.gg>
2021-07-18 13:09:47 -0500

    New style to donations

--- src/components/LiveDonations/LiveDonations.jsx
+++ src/components/LiveDonations/LiveDonations.jsx
@@ -54,11 +54,11 @@ const LiveDonations = () => {
                 const donation = popDonation();
                 setDonationToShow(donation);
                 markDonationAsRead(streamerUid, donation.id);
-            }, 2500);
+            }, 5000);
         } else {
             setTimeout(() => {
                 setDonationToShow(null);
-            }, 2500);
+            }, 5000);
         }
 
         if (!streamerUid) {
@@ -69,14 +69,59 @@ const LiveDonations = () => {
     document.body.style.backgroundColor = 'transparent';
 
     return (
-        <div style={{ backgroundColor: 'transparent', height: '100vh', width: '100vw' }}>
+        <div style={{ backgroundColor: 'transparent', height: '400px', width: '400px', flex: 1 }}>
             {donationToShow &&
                 <>
-                    <QaplaLogo /> {donationToShow.userName} ha donado {donationToShow.amountQoins} Qoins
+                    <DonationHandler donationToShow={donationToShow} />
                 </>
             }
         </div>
     );
+}
+
+const DonationHandler = (donationToShow) => {
+    const donation = donationToShow.donationToShow;
+    useEffect(() => {
+        console.log(donation)
+        console.log(donation.userName)
+    })
+    return (
+        <div style={{ flex: 1, fontFamily: 'Montserrat' }}>
+            <div style={{
+                position: 'absolute',
+                backgroundColor: '#0D1021',
+                alignContent: 'center',
+                justifyItems: 'center',
+                borderRadius: 30,
+                width: '100px',
+                height: '100px',
+                marginLeft: '150px',
+                top: 0
+            }} >
+                <QaplaLogo style={{ alignSelf: 'center', width: '75%', height: '75%', marginTop: '12.5%', marginLeft: '12.5%' }} />
+
+            </div>
+            <div style={{ flex: 1, marginTop: '110px', backgroundColor: '#0D1021', borderRadius: 40, height: '80px' }}>
+                <p style={{ color: '#fff', textAlign: 'center', fontSize: 20, paddingTop: '5px' }}>
+                    <b style={{ color: '#3DF9DF' }}>{donation.userName}</b><br />ha donado<br /><b style={{ color: '#3DF9DF' }}>{donation.amountQoins}</b> Qoins
+                </p>
+                {donation.message !== '' &&
+                    <div style={{ flex: 1 }}>
+                        <div style={{ flex: 1, marginTop: '10px', backgroundColor: '#0D1021', borderRadius: 40, alignSelf: 'center', width: '175px', paddingTop: '2px', paddingBottom: '2px', marginLeft: '112.5px' }}>
+                            <p style={{ color: '#fff', textAlign: 'center', fontSize: 18 }}>
+                                Con el mensaje
+                            </p>
+                        </div>
+                        <div style={{ flex: 1, marginTop: '10px', backgroundColor: '#0D1021', borderRadius: 40, width: '300px', height: 'auto', paddingTop: '1px', paddingBottom: '1px', marginLeft: '50px' }}>
+                            <p style={{ color: '#fff', textAlign: 'center', fontSize: 18 }}>
+                                {donation.message}
+                            </p>
+                        </div>
+                    </div>
+                }
+            </div>
+        </div >
+    )
 }
 
 export default LiveDonations;

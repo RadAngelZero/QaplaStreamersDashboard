bab292b155563949b23b6b6363961365040b670f
DHVSGG <diego@qapla.gg>
2021-07-21 17:00:01 -0500

    Real instructions to add cheers added

--- src/components/Settings/CheersSettings.jsx
+++ src/components/Settings/CheersSettings.jsx
@@ -3,7 +3,10 @@ import { makeStyles, Grid, Card, CardMedia, Tooltip } from '@material-ui/core';
 
 import StreamerTextInput from '../StreamerTextInput/StreamerTextInput';
 import { ReactComponent as CopyIcon } from './../../assets/CopyPaste.svg';
-import Rocket from './../../assets/RocketLeague.jpg';
+import Step1 from './../../assets/addCheersTutorial1.jpg';
+import Step2 from './../../assets/addCheersTutorial2.jpg';
+import Step3 from './../../assets/addCheersTutorial3.jpg';
+import Step4 from './../../assets/addCheersTutorial4.jpg';
 
 const useStyles = makeStyles(() =>({
     instructionsMargin: {
@@ -18,6 +21,9 @@ const useStyles = makeStyles(() =>({
         marginTop: 24,
         fontSize: 16,
         color: '#FFF'
+    },
+    instructionMediaCard: {
+        borderRadius: 20
     },
     link: {
         color: '#6C5DD3',
@@ -41,22 +47,26 @@ const InstructionSection = ({ title, description, mediaContainerComponent = 'img
             <p className={classes.instructionTitle}>
                 {title}
             </p>
-            <p className={classes.instructionDescription}>
-                {description}
-            </p>
-            <Grid container className={classes.instructionsMargin}>
-                <Grid xs={12} sm={8} md={6}>
-                    <Card>
-                        <CardMedia component={mediaContainerComponent}
-                            width='480'
-                            height='270'
-                            src={src}
-                            frameborder='0'
-                            allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
-                            allowfullscreen />
-                    </Card>
+            {description &&
+                <p className={classes.instructionDescription}>
+                    {description}
+                </p>
+            }
+            {src &&
+                <Grid container className={classes.instructionsMargin}>
+                    <Grid xs={12} sm={8} md={7}>
+                        <Card className={classes.instructionMediaCard}>
+                            <CardMedia component={mediaContainerComponent}
+                                width='480'
+                                height='375'
+                                src={src}
+                                frameborder='0'
+                                allow='accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
+                                allowfullscreen />
+                        </Card>
+                    </Grid>
                 </Grid>
-            </Grid>
+            }
         </div>
     );
 };
@@ -77,10 +87,10 @@ const CheersSettings = ({ twitchId }) => {
     return (
         <div className={classes.container}>
             <p className={classes.instructionTitle}>
-                Recibe los mensajes de tu comunidad
+                Receive messages from your community
             </p>
             <p className={classes.instructionDescription}>
-                Muestra los mensajes de los cheers que te envía tu comunidad en tu transmisiones agregando este link a tu OBS.
+                Show the messages of the cheers that your community sends you in your streams by adding this link into your OBS.
             </p>
             <Grid container className={classes.instructionsMargin}>
                 <Grid sm={6} xs={12}>
@@ -95,16 +105,19 @@ const CheersSettings = ({ twitchId }) => {
                         value={cheersURL} />
                 </Grid>
             </Grid>
-            <InstructionSection title='¿Cómo agrego las alertas de cheers de Qapla a mi OBS?'
-                description={<>¡Es muy fácil! Funciona igual que las alertas que ya conoces y tanto te gusta mostrar en tus streams :). <b>Mira este video o bien sigue las sencillas instrucciones</b> de abajo.</>}
-                mediaContainerComponent='iframe'
-                src='https://www.youtube.com/embed/QH2-TGUlwu4' />
-            <InstructionSection title='1. Abre tu setup en tu OBS'
-                description='Ve a tu OBS (StreamLabs, OBS, Stream Elements, etc.) y abre el setup con el que normalmente transmites.'
-                src={Rocket} />
-            <InstructionSection title='2. Abre tu setup en tu OBS'
-                description='Ve a tu OBS (StreamLabs, OBS, Stream Elements, etc.) y abre el setup con el que normalmente transmites.'
-                src={Rocket} />
+            <InstructionSection title='How do I add Qapla cheer alerts to my OBS?'
+                description={<>It is too easy! It works just like the alerts you already know and you like to show so much in your streams :). <b>Follow the simple instructions below to add them.</b></>} />
+            <InstructionSection title='1. Open your OBS setup'
+                description='Go to your OBS (StreamLabs, OBS, Stream Elements, etc.) and open the setup that you use regularly to stream.' />
+            <InstructionSection title='2. Add a source to your screen in your OBS'
+                src={Step1} />
+            <InstructionSection title='3. Select Browser Source from the sources menu'
+                src={Step2} />
+            <InstructionSection title='4. Name your source (i.e Qapla Cheers)'
+                src={Step3} />
+            <InstructionSection title='5. Copy paste your custom Qapla URL.'
+                description='Set both width and height to 400, and uncheck all the other options.'
+                src={Step4} />
             <div className={classes.instructionsMargin} />
         </div>
     );

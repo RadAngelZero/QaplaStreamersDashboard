d06afcf9e5123830242c1f2532dccd7147020f2f
RadAngelQapla <angel@qapla.gg>
2021-10-10 23:39:36 -0500

    redesign Live Donations and patch for cheers balance text on profile

--- src/components/LiveDonations/LiveDonations.jsx
+++ src/components/LiveDonations/LiveDonations.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 
+import styles from './LiveDonations.module.css';
+import { ReactComponent as DonatedQoin } from './../../assets/DonatedQoin.svg';
 import { listenToUserStreamingStatus, getStreamerUidWithTwitchId, listenForUnreadStreamerCheers, markDonationAsRead, removeListenerForUnreadStreamerCheers, listenForTestCheers, removeTestDonation } from '../../services/database';
 import { ReactComponent as QaplaLogo } from './../../assets/QaplaLogo.svg';
 import donationAudio from '../../assets/notification.wav';
@@ -85,7 +87,7 @@ const LiveDonations = () => {
     document.body.style.backgroundColor = 'transparent';
 
     return (
-        <div style={{ backgroundColor: 'transparent', height: '400px', width: '400px', flex: 1 }}>
+        <div style={{ backgroundColor: 'transparent', height: '200px', width: '400px', flex: 1 }}>
             {donationToShow &&
                 <>
                     <DonationHandler donationToShow={donationToShow} />
@@ -99,40 +101,47 @@ const DonationHandler = (donationToShow) => {
     const donation = donationToShow.donationToShow;
 
     return (
-        <div style={{ flex: 1, fontFamily: 'Montserrat' }}>
-            <div style={{
-                position: 'absolute',
-                backgroundColor: '#0D1021',
-                alignContent: 'center',
-                justifyItems: 'center',
-                borderRadius: 30,
-                width: '100px',
-                height: '100px',
-                marginLeft: '150px',
-                top: 0
-            }} >
-                <QaplaLogo style={{ alignSelf: 'center', width: '75%', height: '75%', marginTop: '12.5%', marginLeft: '12.5%' }} />
-
+        <div style={{ display: 'flex', flex: 1, flexDirection: 'column', fontFamily: 'Montserrat', backgroundColor: '#f0f0' }}>
+            <div
+                className={styles.donationBubble}
+                style={{
+                    display: 'flex',
+                    flexDirection: 'row',
+                    justifyContent: 'space-around',
+                    width: 'fit-content',
+                    minWidth: '340px',
+                    height: '60px',
+                    borderRadius: '18px',
+                    padding: '0 18px'
+                }}
+            >
+                <div style={{ display: 'flex', alignSelf: 'center' }}>
+                    <p style={{ display: 'flex', color: 'white', fontSize: '1.05rem', textAlign: 'center' }}>
+                        <b style={{ color: '#09ffd2' }}>{donation.twitchUserName}</b>
+                        <div style={{ margin: '0 6px' }}>ha enviado</div>
+                        <b style={{ color: '#09ffd2' }}>{donation.amountQoins} Qoins</b>
+                    </p>
+                </div>
+                <div style={{ width: '10px' }}></div>
+                <div style={{ display: 'flex', flex: 1, alignSelf: 'center' }}>
+                    <DonatedQoin style={{ display: 'flex', flex: 1 }} />
+                </div>
             </div>
-            <div style={{ flex: 1, marginTop: '110px', backgroundColor: '#0D1021', borderRadius: 40, height: '80px' }}>
-                <p style={{ color: '#fff', textAlign: 'center', fontSize: 20, paddingTop: '5px' }}>
-                    <b style={{ color: '#3DF9DF' }}>{donation.twitchUserName}</b><br />ha donado<br /><b style={{ color: '#3DF9DF' }}>{donation.amountQoins}</b> Qoins
-                </p>
-                {donation.message !== '' &&
-                    <div style={{ flex: 1 }}>
-                        <div style={{ flex: 1, marginTop: '10px', backgroundColor: '#0D1021', borderRadius: 40, alignSelf: 'center', width: '175px', paddingTop: '2px', paddingBottom: '2px', marginLeft: '112.5px' }}>
-                            <p style={{ color: '#fff', textAlign: 'center', fontSize: 18 }}>
-                                Con el mensaje
-                            </p>
-                        </div>
-                        <div style={{ flex: 1, marginTop: '10px', backgroundColor: '#0D1021', borderRadius: 40, width: '300px', height: 'auto', paddingTop: '1px', paddingBottom: '1px', marginLeft: '50px' }}>
-                            <p style={{ color: '#fff', textAlign: 'center', fontSize: 18, maxWidth: '300px' }}>
-                                {donation.message}
-                            </p>
-                        </div>
+            {donation.message !== '' &&
+                <>
+                    <div style={{ display: 'flex', height: '6px' }}></div>
+                    <div style={{
+                        display: 'flex',
+                        width: 'fit-content',
+                        backgroundColor: '#4D00FB',
+                        borderRadius: '18px',
+                        borderBottomLeftRadius: '4px',
+                        padding: '16.3px 20.4px'
+                    }}>
+                        <p style={{ display: 'flex', color: 'white', fontSize: '1rem' }}>{donation.message}</p>
                     </div>
-                }
-            </div>
+                </>
+            }
         </div >
     )
 }

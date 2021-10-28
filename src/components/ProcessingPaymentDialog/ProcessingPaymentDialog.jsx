39bf79507116143e5ba9da59793943ace7af2686
DHVSGG <diego@qapla.gg>
2021-10-28 18:04:13 -0500

    Translations for payment dialog added

--- src/components/ProcessingPaymentDialog/ProcessingPaymentDialog.jsx
+++ src/components/ProcessingPaymentDialog/ProcessingPaymentDialog.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { makeStyles, Dialog, DialogActions, DialogTitle, DialogContent, DialogContentText } from '@material-ui/core';
+import { useTranslation } from 'react-i18next';
+
 import ContainedButton from '../ContainedButton/ContainedButton';
 
 const useStyles = makeStyles(() => ({
@@ -18,6 +20,7 @@ const useStyles = makeStyles(() => ({
 }));
 
 const ProcessingPaymentDialog = ({ open, finished = false, onClose }) => {
+    const { t } = useTranslation();
     const classes = useStyles();
 
     return (
@@ -30,14 +33,15 @@ const ProcessingPaymentDialog = ({ open, finished = false, onClose }) => {
                 paper: classes.paper
             }}>
             <DialogTitle className={classes.whiteColor}>
-                Pago realizado con exito
+                {t('ProcessingPaymentDialog.successfulPayment')}
             </DialogTitle>
             <DialogContent>
                 <DialogContentText className={classes.whiteColor}>
                     {!finished ?
-                        'Espera un momento mientras activamos tu cuenta y arreglamos algunos detalles'
+                        t('ProcessingPaymentDialog.activatingAccount')
                     :
-                        'Listo, tu membresia esta activa, presiona el boton para terminar'
+                        t('ProcessingPaymentDialog.membershipReady')
+
                     }
                 </DialogContentText>
             </DialogContent>
@@ -46,7 +50,7 @@ const ProcessingPaymentDialog = ({ open, finished = false, onClose }) => {
                     <ContainedButton variant='contained'
                         onClick={onClose}
                         className={classes.finishButton}>
-                        Terminar
+                        {t('ProcessingPaymentDialog.finish')}
                     </ContainedButton>
                 }
             </DialogActions>

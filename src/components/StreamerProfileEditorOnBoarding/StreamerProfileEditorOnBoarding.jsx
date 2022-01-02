d9ff0e779b9a39079a8d12dc556ba0d8dd294b49
RadAngelQapla <angel@qapla.gg>
2022-01-02 14:33:56 -0600

    base of tags

--- src/components/StreamerProfileEditorOnBoarding/StreamerProfileEditorOnBoarding.jsx
+++ src/components/StreamerProfileEditorOnBoarding/StreamerProfileEditorOnBoarding.jsx
@@ -93,6 +93,9 @@ const QaplaChip = withStyles(() => ({
 
 const StreamerProfileEditorOnBoarding = ({ user }) => {
     const [isPresentation, setIsPresentation] = useState(false)
+    const [isBioCreation, setIsBioCreation] = useState(false)
+    const [tagSearch, setTagSearch] = useState('')
+    const [bio, setBio] = useState('')
     const [bold, setBold] = useState(false)
     const [italic, setItalic] = useState(false)
     const [underline, setUnderline] = useState(false)
@@ -100,7 +103,6 @@ const StreamerProfileEditorOnBoarding = ({ user }) => {
     const [emoji, setEmoji] = useState(false)
     const [unorderedList, setUnorderedList] = useState(false)
     const [orderedList, setOrderedList] = useState(false)
-    const [bio, setBio] = useState('')
 
     const toggleBold = () => {
         setBold(!bold)
@@ -140,6 +142,11 @@ const StreamerProfileEditorOnBoarding = ({ user }) => {
 
     const laterButtonForm = () => {
 
+    }
+
+    const onTagSearchChange = (e) => {
+        let input = e.target.value
+        setTagSearch(input)
     }
 
     const onBioChange = (e) => {
@@ -202,74 +209,78 @@ const StreamerProfileEditorOnBoarding = ({ user }) => {
                     :
                     <>
                         <p className={styles.modalTextHeader} style={{ marginTop: '52px' }}>
-                            Preséntate con la comunidad
+                            {isBioCreation ? 'Tags' : 'Preséntate con la comunidad'}
                         </p>
-                        <p className={styles.modalTextSubParagraph} style={{ marginTop: '17px' }}>
-                            Tu intro es un vistazo de ti mismo y tu contenido.
-                            Hazlo ameno 😉
+                        <p className={styles.modalTextSubParagraph} style={{ marginTop: '17px', width: isBioCreation ? '70%' : '60%' }}>
+                            {isBioCreation ? 'Agrega etiquetas que te representen a ti como creador, tu persona, tu contenido, etc.' : 'Tu intro es un vistazo de ti mismo y tu contenido. Hazlo ameno 😉'}
                         </p>
-                        <div style={{
-                            display: 'flex',
-                            flexDirection: 'column',
-                            backgroundColor: '#202750',
-                            borderRadius: '15px',
-                            width: '390px',
-                            height: '234px',
-                            marginTop: '20px',
-                            overflow: 'hidden'
-                        }}>
-                            <div style={{
-                                display: 'flex',
-                                height: '46px',
-                                width: '100%'
-                            }}>
-                                <FormattingButton onClick={toggleBold}>
-                                    <div style={{ backgroundColor: bold ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
-                                        <BoldIcon />
+                        {isBioCreation ?
+                            <>
+
+                                <div className={styles.modalBioEditorContainer}>
+                                    <div className={styles.modalBioEditorFormatterButtonsContainer}>
+                                        <FormattingButton onClick={toggleBold}>
+                                            <div style={{ backgroundColor: bold ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
+                                                <BoldIcon />
+                                            </div>
+                                        </FormattingButton>
+                                        <FormattingButton onClick={toggleItalic}>
+                                            <div style={{ backgroundColor: italic ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
+                                                <ItalicIcon />
+                                            </div>
+                                        </FormattingButton>
+                                        <FormattingButton onClick={toggleUnderline}>
+                                            <div style={{ backgroundColor: underline ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
+                                                <UnderlineIcon />
+                                            </div>
+                                        </FormattingButton>
+                                        <FormattingButton onClick={toggleStrikeThrough}>
+                                            <div style={{ backgroundColor: strikeThrough ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
+                                                <StrikeThroughIcon />
+                                            </div>
+                                        </FormattingButton>
+                                        <FormattingButton onClick={toggleEmoji}>
+                                            <div style={{ backgroundColor: emoji ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
+                                                <EmojiIcon />
+                                            </div>
+                                        </FormattingButton>
+                                        <FormattingButton onClick={toggleUnorderedList}>
+                                            <div style={{ backgroundColor: unorderedList ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
+                                                <UnorderedListIcon />
+                                            </div>
+                                        </FormattingButton>
+                                        <FormattingButton onClick={toggleOrderedList}>
+                                            <div style={{ backgroundColor: orderedList ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
+                                                <OrderedListIcon />
+                                            </div>
+                                        </FormattingButton>
                                     </div>
-                                </FormattingButton>
-                                <FormattingButton onClick={toggleItalic}>
-                                    <div style={{ backgroundColor: italic ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
-                                        <ItalicIcon />
-                                    </div>
-                                </FormattingButton>
-                                <FormattingButton onClick={toggleUnderline}>
-                                    <div style={{ backgroundColor: underline ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
-                                        <UnderlineIcon />
-                                    </div>
-                                </FormattingButton>
-                                <FormattingButton onClick={toggleStrikeThrough}>
-                                    <div style={{ backgroundColor: strikeThrough ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
-                                        <StrikeThroughIcon />
-                                    </div>
-                                </FormattingButton>
-                                <FormattingButton onClick={toggleEmoji}>
-                                    <div style={{ backgroundColor: emoji ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
-                                        <EmojiIcon />
-                                    </div>
-                                </FormattingButton>
-                                <FormattingButton onClick={toggleUnorderedList}>
-                                    <div style={{ backgroundColor: unorderedList ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
-                                        <UnorderedListIcon />
-                                    </div>
-                                </FormattingButton>
-                                <FormattingButton onClick={toggleOrderedList}>
-                                    <div style={{ backgroundColor: orderedList ? '#57758c' : '#0000' }} className={styles.modalButtonActiveIndicator}>
-                                        <OrderedListIcon />
-                                    </div>
-                                </FormattingButton>
-                            </div>
-                            <StreamerTextInput
-                                containerClassName={styles.modalTextInputContainer}
-                                textInputClassName={styles.modalTextInput}
-                                rows={10}
-                                rowsMax={10}
-                                value={bio}
-                                onChange={onBioChange}
-                                fullWidth
-                                multiline
-                            />
-                        </div>
+                                    <StreamerTextInput
+                                        containerClassName={styles.modalBioTextInputContainer}
+                                        textInputClassName={styles.modalBioTextInput}
+                                        rows={10}
+                                        rowsMax={10}
+                                        value={bio}
+                                        onChange={onBioChange}
+                                        fullWidth
+                                        multiline
+                                    />
+                                </div>
+                            </>
+                            :
+                            <>
+                                <StreamerTextInput
+                                    containerClassName={styles.modalTagSearchContainer}
+                                    textInputClassName={styles.modalTagSearchTextInput}
+                                    value={tagSearch}
+                                    onChange={onTagSearchChange}
+                                    placeholder={'Busca o crea un tag'}
+                                    fullWidth
+                                />
+                                
+                            </>
+                        }
+
                         <ContainedButton onClick={continueButtonForm} className={styles.modalButtonFormContinue}>
                             Continuar
                         </ContainedButton>

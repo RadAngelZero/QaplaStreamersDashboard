3a5781d57332d05056c536ca68f933dcad24bf87
DHVSGG <diego@qapla.gg>
2022-05-17 17:11:38 -0500

    Margins on Q Store

--- src/components/QStore/QStore.jsx
+++ src/components/QStore/QStore.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, Card, CardActionArea, CardActions, CardMedia, Grid } from '@material-ui/core';
+import { Button, Card, CardActionArea, CardActions, CardMedia, Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 import { useTranslation } from 'react-i18next';
 
@@ -10,7 +10,8 @@ const useStyles = makeStyles({
     container: {
         paddingLeft: 92,
         paddingTop: 60,
-        paddingRight: 24
+        paddingRight: 24,
+        marginLeft: 0
     },
     title: {
         fontSize: 30,
@@ -32,8 +33,9 @@ const useStyles = makeStyles({
         backgroundSize: 'cover'
     },
     card: {
+        maxWidth: 200,
         backgroundColor: 'transparent',
-        boxShadow: 0,
+        boxShadow: 'none',
     },
     productButton: {
         background: 'linear-gradient(0deg, #3B4BF9, #3B4BF9), #FF006B',
@@ -44,7 +46,8 @@ const useStyles = makeStyles({
         marginTop: 35,
         color: '#FFF',
         fontFamily: 'Inter',
-        borderRadius: 8
+        borderRadius: 8,
+        textTransform: 'none'
     },
     goalButton: {
         background: 'linear-gradient(0deg, #00FFDD, #00FFDD), linear-gradient(0deg, #3B4BF9, #3B4BF9), #FF006B',
@@ -55,7 +58,8 @@ const useStyles = makeStyles({
         marginTop: 35,
         color: '#0D1021',
         fontFamily: 'Inter',
-        borderRadius: 8
+        borderRadius: 8,
+        textTransform: 'none'
     }
 });
 
@@ -64,13 +68,13 @@ const ProductCard = ({ image }) => {
     const { t } = useTranslation();
 
     return (
-        <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+        <Grid item xs={12} sm={6} md={3}>
             <Card classes={{ root: classes.card }}>
                 <CardActionArea>
                     <CardMedia className={classes.media} image={image} />
                 </CardActionArea>
                 <CardActions>
-                    <Button size='large' variant='contained' classes={{ root: classes.productButton }}>
+                    <Button target='_blank' href='https://discord.gg/q7tc2A6z' size='large' variant='contained' classes={{ root: classes.productButton }}>
                         {t('QStore.acquire')}
                     </Button>
                 </CardActions>
@@ -98,7 +102,8 @@ const QStore = ({ user }) => {
     }, [storeItems]);
 
     return (
-        <StreamerDashboardContainer user={user} containerStyle={classes.container}>
+        <StreamerDashboardContainer user={user}>
+            <Container maxWidth='lg' className={classes.container}>
             {user ?
                 <Grid container spacing={8}>
                     <Grid item xs={12}>
@@ -109,13 +114,13 @@ const QStore = ({ user }) => {
                             {t('QStore.description')}
                         </p>
                     </Grid>
-                    <Grid item xs={12} sm={6} md={4} lg={3} xl={2}>
+                    <Grid item xs={12} sm={6} md={3}>
                         <Card classes={{ root: classes.card }}>
                             <CardActionArea>
                                 <CardMedia className={classes.media} image='https://imageio.forbes.com/specials-images/imageserve/5fd2263efcf061ccad6f7d95/0x0.jpg?format=jpg&width=1200' />
                             </CardActionArea>
                             <CardActions>
-                                <Button size='large' variant='contained' classes={{ root: classes.goalButton }}>
+                                <Button target='_blank' href='https://discord.gg/q7tc2A6z' size='large' variant='contained' classes={{ root: classes.goalButton }}>
                                     {t('QStore.setGoal')}
                                 </Button>
                             </CardActions>
@@ -128,6 +133,7 @@ const QStore = ({ user }) => {
             :
                 null
             }
+            </Container>
         </StreamerDashboardContainer>
     );
 }

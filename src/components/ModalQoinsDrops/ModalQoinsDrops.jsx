dbc74dd520c524ab27ef7656b7ce3d3b4f545982
jose <joseandrescolmenares02@gmail.com>
2022-08-29 15:46:37 -0300

    LeftSideModal

--- src/components/ModalQoinsDrops/ModalQoinsDrops.jsx
+++ src/components/ModalQoinsDrops/ModalQoinsDrops.jsx
@@ -1,8 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 import style from "./ModalQoinsDrops.module.css";
 
+
+
+import {ReactComponent as  CheckedIcon}  from "../../assets/Unchecked.svg";
+import { ReactComponent as UncheckedIcon }  from "../../assets/TickSquare.svg";
+import  Checkeck  from "../../assets/TickSquareDark.svg";
+
+import {
+  FormControlLabel,
+  makeStyles,
+  Radio,
+  RadioGroup,
+} from "@material-ui/core";
+
+
+
+const useStyles = makeStyles((theme) => ({
+  label: {
+    fontfamily: "Inter",
+    fontstyle: "normal",
+    fontweight: "400",
+    fontSize: "13px",
+    color: "rgba(255, 255, 255, 0.603447)",
+  },
+
+}));
+
+
+
+
 const ModalQoinsDrops = () => {
+  const [loanding, setLoading] = useState(false);
+  const [loadingEnd, setLoadingEnd] = useState(false);
+  const classes = useStyles();
+
+  
+
+  const handleOnClick = () => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoadingEnd(true);
+    }, 4000);
+  };
+
+
+
+
   return (
     <div className={style.containerModal}>
       <div className={style.container}>
@@ -13,46 +58,94 @@ const ModalQoinsDrops = () => {
               Set a time for the Qoins' channel reward to show on yor stream
             </p>
           </div>
-          <div className={style.content_input}>
-            <label>
-              <input type="checkbox" />
-              Right away
-            </label>
-
-            <label>
-              <input type="checkbox" />
-              in 30 min
-            </label>
-
-            <label>
-              <input type="checkbox" />
-              in 45 min
-            </label>
-
-            <label>
-              <input type="checkbox" />
-              in 1 hour
-            </label>
-          </div>
-          <button className={style.button}>Enable Drops</button>
-        </div>
-        <div className={style.secctions_text}>
-          <div className={style.Titulo_secction_text}>
-            <h1>💬 Send text to followers</h1>
-            <p>
-              avisa a tu gente algo relevante sobre tu stream, crea hype o lo
-              que tu quieras! Tus seguidores reciben una notificacion movil con
-              tu mensaje.
-            </p>
-          </div>
-          <input className={style.input_grande} type="text" />
-          <div className={style.contenedor_boton}>
-            <button>Send</button>
-          </div>
+          {!loadingEnd ? (
+            <>
+              
+                <RadioGroup
+                  aria-labelledby="demo-radio-buttons-group-label"
+                  name="radio-buttons-group"
+                >
+                  <FormControlLabel
+                    value="Right away"
+                    classes={{ label: classes.label }}
+                    control={
+                      <Radio
+                      classes={{Radio: classes.Radio}}
+                        defaultChecked
+                        checkedIcon={<UncheckedIcon />}
+                        icon={<CheckedIcon />}
+                        style={{ backgroundColor: "transparent" }}
+                      />
+                    }
+                    label="Right away"
+                  />
+                  <FormControlLabel
+                    value="in 30 min"
+                    classes={{ label: classes.label }}
+                    control={
+                      <Radio
+                        defaultChecked
+                        checkedIcon={<UncheckedIcon />}
+                        icon={<CheckedIcon />}
+                        style={{ backgroundColor: "transparent" }}
+                      />
+                    }
+                    label="in 30 min"
+                  />
+                  <FormControlLabel
+                    value="in 45 min"
+                    classes={{ label: classes.label }}
+                    control={
+                      <Radio
+                        defaultChecked
+                        checkedIcon={<UncheckedIcon />}
+                        icon={<CheckedIcon />}
+                        style={{ backgroundColor: "transparent" }}
+                      />
+                    }
+                    label="in 45 min"
+                  />
+                  <FormControlLabel
+                    value="in 1 hour"
+                    classes={{ label: classes.label }}
+                    control={
+                      <Radio
+                        defaultChecked
+                        checkedIcon={<UncheckedIcon />}
+                        icon={<CheckedIcon />}
+                        style={{ backgroundColor: "transparent" }}
+                      />
+                    }
+                    label="in 1 hour"
+                  />
+                </RadioGroup>
+              
+              <div>
+                {!loanding ? (
+                  <button onClick={handleOnClick} className={style.button}>
+                    Enable Drops
+                  </button>
+                ) : (
+                  <div className={style.container_loader}>
+                    
+                    <p>Setting up drops</p>
+                    <div className={style.loader}></div>
+                  </div>
+                )}
+              </div>{" "}
+            </>
+          ) : (
+            <div className={style.loading_End}>
+              <img src={Checkeck} alt="icons"/>
+              <p>Qoins will drop in 30 min</p>
+            </div>
+          )}
         </div>
       </div>
 
-      <p className={style.extraData}>Exploring Astraland 🌙 /30 Apri/10:30 p.m.</p>
+      <p className={style.extraData}>
+        Exploring Astraland 🌙 /30 Apri/10:30 p.m.
+      </p>
     </div>
   );
 };

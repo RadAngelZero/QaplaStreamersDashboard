81234e3e91ea19d027c00c0b64a14551b2761564
jose <joseandrescolmenares02@gmail.com>
2022-09-12 18:06:23 -0300

    arreglos en BarProgressBit

--- src/components/BarProgressBit/BarProgressBit.jsx
+++ src/components/BarProgressBit/BarProgressBit.jsx
@@ -1,14 +1,14 @@
- import React, { useState, useEffect} from "react";
- import { useTranslation } from 'react-i18next';
- import style from "./BarProgressBit.module.css";
+import React, { useState, useEffect } from "react";
+import { useTranslation } from "react-i18next";
+import style from "./BarProgressBit.module.css";
 
- import { withStyles } from "@material-ui/core/styles";
- import LinearProgress from "@material-ui/core/LinearProgress";
- import { Dialog, makeStyles } from "@material-ui/core";
- import Button from '@material-ui/core/Button';
+import { withStyles } from "@material-ui/core/styles";
+import LinearProgress from "@material-ui/core/LinearProgress";
+import { Dialog, makeStyles } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
- import CasthQutDialog from "../CasthQutDialog/CasthQutDialog";
- import CasthQutConfirmDialog from '../CasthQutConfirmDialog/CasthQutConfirmDialog'
+import CasthQutDialog from "../CasthQutDialog/CasthQutDialog";
+import CasthQutConfirmDialog from "../CasthQutConfirmDialog/CasthQutConfirmDialog";
 
 const BorderLinearProgress = withStyles((theme) => ({
   root: {
@@ -24,7 +24,7 @@ const BorderLinearProgress = withStyles((theme) => ({
 }))(LinearProgress);
 
 const useStyles = makeStyles((theme) => ({
-  dialogContainer: {
+    dialogContainer: {
     backdropFilter: "blur(20px)",
   },
   dialogRoot: {},
@@ -34,109 +34,116 @@ const useStyles = makeStyles((theme) => ({
     overflow: "visible",
     borderRadius: "35px",
   },
-
 }));
+
 const ContButton = withStyles((theme) => ({
   root: {
-    backgroundColor: '#3B4BF9',
-    fontSize:'12px',
-    height: '40px',
-    maxWidth: '90px',
-    minWidth: '90px',
-    fontWeight:'500',
-    padding: '16px',
-    color: '#FFFFFF',
-    textTransform: 'none',
-    textAlign: 'center',
-    '&:hover': {
-      backgroundColor: '#3B4BF9'
+    backgroundColor: "#3B4BF9",
+    fontSize: "12px",
+    height: "40px",
+    maxWidth: "90px",
+    minWidth: "90px",
+    fontWeight: "500",
+    padding: "16px",
+    color: "#FFFFFF",
+    textTransform: "none",
+    textAlign: "center",
+    "&:hover": {
+        backgroundColor: "#3B4BF9",
     },
-    disabled:{
-      color: '#FFFFFF',
+    disabled: {
+        color: "#FFFFFF",
     },
-    '&:disabled': {
-      color: '#ffff',
-      opacity:'0.4'
+    "&:disabled": {
+        color: "#ffff",
+        opacity: "0.4",
     },
   },
- 
-
-  
 }))(Button);
 
-
-
-const BarProgressBit = ({ user, estimatedBits, availableBits, nextMilestone }) => {
-  const classes = useStyles();
+const BarProgressBit = ({
+    user,
+    estimatedBits,
+    availableBits,
+    nextMilestone,
+}) => {
   const [open, setOpen] = useState(false);
   const [confirmCashOut, setConfirmCashOut] = useState(false);
   const [openConfirm, setOpenConfirm] = useState(false);
   const [disabledButon, setDisabledButon] = useState(false);
+  const classes = useStyles();
   const { t } = useTranslation();
+
+  useEffect(() => {
+    if (availableBits <= 0) {
+        setDisabledButon(true);
+    } else {
+        setDisabledButon(false);
+    }
+  }, [disabledButon, availableBits]);
 
   const handleOpenDialod = () => {
     setOpen(true);
   };
 
-  useEffect(() => {
-    if (availableBits <= 0){
-      setDisabledButon(true);
-    }
-    else {
-      setDisabledButon(false);
-    }
-  },[disabledButon, availableBits])
-
-  const milestoneProgress = ((250 - (nextMilestone - estimatedBits)) / 250) * 100;
+  const milestoneProgress =
+    ((250 - (nextMilestone - estimatedBits)) / 250) * 100;
 
   return (
     <>
-      <div className={style.container}>
-        <div className={style.barProgress}>
-          <div className={style.titulos}>
-            <p className={style.titulo_Porcentaje}>
-              {t('StreamerProfile.BarProgressBit.nextMilestone')}
-            </p>
-            <p className={style.porcentaje}>
-              {estimatedBits.toLocaleString()} / {nextMilestone.toLocaleString()}
-            </p>
-          </div>
-          <BorderLinearProgress variant="determinate" value={milestoneProgress} />
+        <div className={style.container}>
+            <div className={style.barProgress}>
+                <div className={style.titulos}>
+                    <p className={style.percentageTitle}>
+                        {t("StreamerProfile.BarProgressBit.nextMilestone")}
+                    </p>
+                    <p className={style.porcentaje}>
+                                    {estimatedBits.toLocaleString()} /{" "}
+                                    {nextMilestone.toLocaleString()}
+                    </p>
+                </div>
+                <BorderLinearProgress
+                    variant="determinate"
+                    value={milestoneProgress}/>
+            </div>
+            <div className={style.puntos}>
+                <p>{t("StreamerProfile.BarProgressBit.available")}</p>
+                <h2>{availableBits.toLocaleString()}</h2>
+            </div>
         </div>
-        <div className={style.puntos}>
-          <p>
-            {t('StreamerProfile.BarProgressBit.available')}
-          </p>
-          <h2>
-            {availableBits.toLocaleString()}
-          </h2>
-        </div>
-      </div>
-      <ContButton  disabled={disabledButon} onClick={() =>{ handleOpenDialod(); setConfirmCashOut(false) }}>
-        {t('StreamerProfile.BarProgressBit.cashOut')}
-      </ContButton>
-      {!confirmCashOut ? (
+        <ContButton
+            disabled={disabledButon}
+            onClick={() => {
+                handleOpenDialod();
+                setConfirmCashOut(false);
+        }}
+      >
+            {t("StreamerProfile.BarProgressBit.cashOut")}
+        </ContButton>
+        {!confirmCashOut ? (
+          <CasthQutDialog
+            open={open}
+            onClose={() => setOpen(false)}
+            user={user}
+            amountBits={availableBits}
+            setOpen={setOpen}
+            setOpenConfirm={setOpenConfirm}
+            setConfirmCashOut={setConfirmCashOut}
+          />
+      ) : (
         <Dialog
-          onClose={() => setOpen(false)}
-          open={open}
-          classes={{
+            onClose={() => setOpenConfirm(false)}
+            open={openConfirm}
+            classes={{
             container: classes.dialogContainer,
             root: classes.dialogRoot,
             paper: classes.paper,
           }}
         >
-          <CasthQutDialog user={user}
-            amountBits={availableBits}
-            setOpen={setOpen}
-            setOpenConfirm={setOpenConfirm}
-            setConfirmCashOut={setConfirmCashOut} />
-        </Dialog>
-      ) : (
-        <Dialog onClose={() => setOpenConfirm(false)} open={openConfirm} classes={{
-          container: classes.dialogContainer,
-          root: classes.dialogRoot,
-          paper: classes.paper}}>
-          <CasthQutConfirmDialog amountBits={availableBits} setOpenConfirm={setOpenConfirm}/>
+          <CasthQutConfirmDialog
+                amountBits={availableBits}
+                setOpenConfirm={setOpenConfirm}
+            />
         </Dialog>
       )}
     </>
--- src/components/CasthQutDialog/CasthQutDialog.jsx
+++ src/components/CasthQutDialog/CasthQutDialog.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
+import { Dialog } from "@material-ui/core";
 
 import style from "./CasthQutDialog.module.css";
 
@@ -7,7 +8,7 @@ import { ReactComponent as BitsIcon } from "./../../assets/BitsIcon.svg";
 import { notifyCashOutToQaplaAdmin } from "../../services/discord";
 import { saveStreamerCashOutRequest } from "../../services/database";
 
-const CasthQutDialog = ({ user, setOpen, setOpenConfirm, setConfirmCashOut, amountBits }) => {
+const CasthQutDialog = ({ user, setOpen, setOpenConfirm, setConfirmCashOut, amountBits, open, onClose }) => {
   const [bits, setBits] = useState(0);
   const [disableButton, setDisableButton] = useState(false);
   const { t } = useTranslation();
@@ -30,26 +31,35 @@ const CasthQutDialog = ({ user, setOpen, setOpenConfirm, setConfirmCashOut, amou
   }
 
   return (
-    <div className={style.container_Dialog}>
-      <div className={style.container}>
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <BitsIcon style={{ width: "35px", height: "35px" }} />
-          <h1>
-            {bits.toLocaleString()}
-          </h1>
+    <Dialog
+          onClose={onClose}
+          open={open}
+          classes={{
+          container: classes.dialogContainer,
+          root: classes.dialogRoot,
+          paper: classes.paper,
+    }}>
+      <div className={style.container_Dialog}>
+        <div className={style.container}>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <BitsIcon style={{ width: "35px", height: "35px" }} />
+            <h1>
+              {bits.toLocaleString()}
+            </h1>
+          </div>
+          <p className={style.calculated_Bit}>200 Qoins = 10 Bits</p>
+          <p className={style.text}>
+            {t('StreamerProfile.BarProgressBit.confirmCashOut')}
+          </p>
+          <button disabled={disableButton} onClick={confirmCashOut} className={style.button_cash}>
+            {t('StreamerProfile.BarProgressBit.cashOut')}
+          </button>
+          <button onClick={() => setOpen(false)} className={style.button_cancel}>
+            {t('StreamerProfile.BarProgressBit.cancel')}
+          </button>
         </div>
-        <p className={style.calculated_Bit}>200 Qoins = 10 Bits</p>
-        <p className={style.text}>
-          {t('StreamerProfile.BarProgressBit.confirmCashOut')}
-        </p>
-        <button disabled={disableButton} onClick={confirmCashOut} className={style.button_cash}>
-          {t('StreamerProfile.BarProgressBit.cashOut')}
-        </button>
-        <button onClick={() => setOpen(false)} className={style.button_cancel}>
-          {t('StreamerProfile.BarProgressBit.cancel')}
-        </button>
       </div>
-    </div>
+    </Dialog>
   );
 };
 

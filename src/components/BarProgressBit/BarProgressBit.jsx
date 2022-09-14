23d8065085bedeb11513acff778c197e1eef9b54
DHVSGG <diego@qapla.gg>
2022-09-14 08:41:45 -0500

    Code fixes

--- src/components/BarProgressBit/BarProgressBit.jsx
+++ src/components/BarProgressBit/BarProgressBit.jsx
@@ -4,7 +4,6 @@ import style from "./BarProgressBit.module.css";
 
 import { withStyles } from "@material-ui/core/styles";
 import LinearProgress from "@material-ui/core/LinearProgress";
-import { Dialog, makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 
 import CasthQutDialog from "../CasthQutDialog/CasthQutDialog";
@@ -24,66 +23,60 @@ const BorderLinearProgress = withStyles((theme) => ({
 }))(LinearProgress);
 
 const ContButton = withStyles((theme) => ({
-  root: {
-    backgroundColor: "#3B4BF9",
-    fontSize: "12px",
-    height: "40px",
-    maxWidth: "90px",
-    minWidth: "90px",
-    fontWeight: "500",
-    padding: "16px",
-    color: "#FFFFFF",
-    textTransform: "none",
-    textAlign: "center",
-    "&:hover": {
+    root: {
         backgroundColor: "#3B4BF9",
-    },
-    disabled: {
+        fontSize: "12px",
+        height: "40px",
+        maxWidth: "90px",
+        minWidth: "90px",
+        fontWeight: "500",
+        padding: "16px",
         color: "#FFFFFF",
+        textTransform: "none",
+        textAlign: "center",
+        "&:hover": {
+            backgroundColor: "#3B4BF9",
+        },
+        disabled: {
+            color: "#FFFFFF",
+        },
+        "&:disabled": {
+            color: "#ffff",
+            opacity: "0.4",
+        },
     },
-    "&:disabled": {
-        color: "#ffff",
-        opacity: "0.4",
-    },
-  },
 }))(Button);
 
-const BarProgressBit = ({
-    user,
-    estimatedBits,
-    availableBits,
-    nextMilestone,
-}) => {
-  const [open, setOpen] = useState(false);
-  const [confirmCashOut, setConfirmCashOut] = useState(false);
-  const [openConfirm, setOpenConfirm] = useState(false);
-  const [disabledButon, setDisabledButon] = useState(false);
-  const { t } = useTranslation();
+const BarProgressBit = ({ user, estimatedBits, availableBits, nextMilestone }) => {
+    const [open, setOpen] = useState(false);
+    const [confirmCashOut, setConfirmCashOut] = useState(false);
+    const [openConfirm, setOpenConfirm] = useState(false);
+    const [disabledButon, setDisabledButon] = useState(false);
+    const { t } = useTranslation();
 
-  useEffect(() => {
-    if (availableBits <= 0) {
-        setDisabledButon(true);
-    } else {
-        setDisabledButon(false);
-    }
-  }, [disabledButon, availableBits]);
+    useEffect(() => {
+        if (availableBits <= 0) {
+            setDisabledButon(true);
+        } else {
+            setDisabledButon(false);
+        }
+    }, [disabledButon, availableBits]);
 
-  const handleOpenDialod = () => {
-    setOpen(true);
-  };
+    const handleOpenDialod = () => {
+        setOpen(true);
+    };
 
-  const milestoneProgress =
-    ((250 - (nextMilestone - estimatedBits)) / 250) * 100;
+    const milestoneProgress = ((250 - (nextMilestone - estimatedBits)) / 250) * 100;
 
-  return (
+    return (
     <>
         <div className={style.container}>
             <div className={style.barProgress}>
-                <div className={style.titulos}>
+                <div className={style.titles}>
                     <p className={style.percentageTitle}>
                         {t("StreamerProfile.BarProgressBit.nextMilestone")}
                     </p>
-                    <p className={style.porcentaje}>
+                    <p className={style.percentage}>
                                     {estimatedBits.toLocaleString()} /{" "}
                                     {nextMilestone.toLocaleString()}
                     </p>
@@ -92,7 +85,7 @@ const BarProgressBit = ({
                     variant="determinate"
                     value={milestoneProgress}/>
             </div>
-            <div className={style.puntos}>
+            <div className={style.available}>
                 <p>{t("StreamerProfile.BarProgressBit.available")}</p>
                 <h2>{availableBits.toLocaleString()}</h2>
             </div>
@@ -107,28 +100,25 @@ const BarProgressBit = ({
             {t("StreamerProfile.BarProgressBit.cashOut")}
         </ContButton>
         {!confirmCashOut ? (
-          <CasthQutDialog
-            open={open}
-            onClose={() => setOpen(false)}
-            user={user}
-            amountBits={availableBits}
-            setOpen={setOpen}
-            setOpenConfirm={setOpenConfirm}
-            setConfirmCashOut={setConfirmCashOut}
-          />
-      ) : (
-        <CasthQutDialog
-            onClose={() => setOpenConfirm(false)}
-            open={openConfirm}
-        >
-          <CasthQutConfirmDialog
+            <CasthQutDialog
+                open={open}
+                onClose={() => setOpen(false)}
+                user={user}
+                amountBits={availableBits}
+                setOpen={setOpen}
+                setOpenConfirm={setOpenConfirm}
+                setConfirmCashOut={setConfirmCashOut}
+            />
+        ) : (
+            <CasthQutConfirmDialog
+                onClose={() => setOpenConfirm(false)}
+                open={openConfirm}
                 amountBits={availableBits}
                 setOpenConfirm={setOpenConfirm}
             />
-        </CasthQutDialog>
-      )}
+        )}
     </>
-  );
+    );
 };
 
 export default BarProgressBit;
